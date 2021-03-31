38f9ad9021339291b900877e1cb7f990312272a5
EloiM2021 <81677241+EloiM2021@users.noreply.github.com>
2021-03-31 12:48:23 +0200

    TABLEAU

--- Form1.vb
+++ Form1.vb
@@ -1,4 +1,13 @@
 ﻿Public Class Form1
+    Dim board(,) As String =
+        {{"bR", "bN", "bB", "bQ", "bK", "bB", "bN", "bR"},
+        {"bp", "bp", "bp", "bp", "bp", "bp", "bp", "bp"},
+        {"__", "__", "__", "__", "__", "__", "__", "__"},
+        {"__", "__", "__", "__", "__", "__", "__", "__"},
+        {"__", "_", "__", "__", "__", "__", "__", "__"},
+        {"__", "__", "_", "__", "__", "__", "__", "__"},
+        {"wp", "wp", "wp", "wp", "wp", "wp", "wp", "wp"},
+        {"wR", "wN", "wB", "wQ", "wK", "wB", "wK", "wR"}}
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
 
     End Sub
