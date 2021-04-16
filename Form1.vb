d5f27bde636060ac8084488530f62ea73f685252
EloiM2021 <81677241+EloiM2021@users.noreply.github.com>
2021-04-16 13:09:08 +0200

    pion

--- Form1.vb
+++ Form1.vb
@@ -18,5 +18,37 @@
         tbl_tableau.BackgroundImage = jeudechec.My.Resources.echecdamier
         tbl_tableau.Top = 70
         tbl_tableau.Left = 510
+        tbl_tableau.BackgroundImageLayout = ImageLayout.Stretch
+
     End Sub
+
+    Public Class pion
+        Public position As Integer()
+        Public deplacement As Integer()
+        Public couleur As String
+
+        Public Sub New(pos As Integer(), col As String)
+            position = pos
+            couleur = col
+        End Sub
+
+        Public Sub SetPosition(pos As Integer())
+            position = pos
+        End Sub
+
+        Public Function GetPosition() As Integer()
+            Return position
+        End Function
+
+        Public Sub SetCouleur(col As String)
+            couleur = col
+        End Sub
+
+        Public Function GetCouleur() As String
+            Return couleur
+        End Function
+
+
+
+    End Class
 End Class
