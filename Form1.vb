7ff30c1b43d566dbfb5e53de8df024e306028601
hh1ll <achillebataille@gmail.com>
2021-04-20 15:59:07 +0200

    dim pion

--- Form1.vb
+++ Form1.vb
@@ -1,5 +1,12 @@
 ﻿Public Class Form1
+    Dim whatobject As Object
     Dim pionblanc1 As pion = New pion({0, 1}, "blanc")
+    Dim pionnoir1 As pion = New pion({0, 6}, "noir")
+
+    'test guthub
+
+
+
     Dim pb As New PictureBox
     Dim board(,) As Integer =
         {{3, 2, 4, 5, 6, 4, 2, 3},
@@ -20,54 +27,132 @@
     'roi : 6
 
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-        Label2.Text = "bonjour"
-        pb.BackgroundImage = jeudechec.My.Resources.wp
         Me.Height = 1080
         Me.Width = 1920
         Me.FormBorderStyle = FormBorderStyle.FixedSingle
-        tbl_tableau.Width = 900
-        tbl_tableau.Height = 900
-        tbl_tableau.RowCount = 8
-        tbl_tableau.ColumnCount = 8
-        tbl_tableau.BackgroundImageLayout = ImageLayout.Stretch
-        tbl_tableau.Top = 70
-        tbl_tableau.Left = 510
-        tbl_tableau.Controls.Add(Label2, 2, 2)
-        tbl_tableau.ForeColor = Color.Blue
-        pionblanc1.SetPosition = board.
-
+        'jeudechec.My.Resources.wp
 
     End Sub
 
+    Private Function test_positions()
+        Select Case whatobject
+            Case 1
+                If whatobject = pionblanc1 Then
 
+                End If
 
-    Public Class pion
-        Public position As Integer()
-        Public deplacement As Integer()
-        Public couleur As String
+            Case 2
 
-        Public Sub New(pos As Integer(), col As String)
-            position = pos
-            couleur = col
-        End Sub
+            Case 3
 
-        Public Sub SetPosition(pos As Integer())
-            position = pos
-        End Sub
+            Case 4
 
-        Public Function GetPosition() As Integer()
-            Return position
-        End Function
+            Case 5
 
-        Public Sub SetCouleur(col As String)
-            couleur = col
-        End Sub
+            Case 6
 
-        Public Function GetCouleur() As String
-            Return couleur
-        End Function
+        End Select
+    End Function
 
+End Class
+Public Class pion
+    Public position As Integer()
+    Public deplacement As Integer()
+    Public couleur As String
 
+    Public Sub New(pos As Integer(), col As String)
+        position = pos
+        couleur = col
+    End Sub
 
-    End Class
+    Public Sub SetPosition(pos As Integer())
+        position = pos
+    End Sub
+
+    Public Function GetPosition() As Integer()
+        Return position
+    End Function
+
+    Public Sub SetCouleur(col As String)
+        couleur = col
+    End Sub
+
+    Public Function GetCouleur() As String
+        Return couleur
+    End Function
+End Class
+
+Public Class cavalier
+    Public deplacement As Integer()
+    Public couleur As String
+
+    Public Sub New(col As String)
+        couleur = col
+    End Sub
+    Public Sub SetCouleur(col As String)
+        couleur = col
+    End Sub
+    Public Function Getcouleur() As String
+        Return couleur
+    End Function
+End Class
+
+Public Class tour
+    Public deplacement As Integer()
+    Public couleur As String
+
+    Public Sub New(col As String)
+        couleur = col
+    End Sub
+    Public Sub SetCouleur(col As String)
+        couleur = col
+    End Sub
+    Public Function Getcouleur() As String
+        Return couleur
+    End Function
+End Class
+
+Public Class fou
+    Public deplacement As Integer()
+    Public couleur As String
+
+    Public Sub New(col As String)
+        couleur = col
+    End Sub
+    Public Sub SetCouleur(col As String)
+        couleur = col
+    End Sub
+    Public Function Getcouleur() As String
+        Return couleur
+    End Function
+End Class
+
+Public Class dame
+    Public deplacement As Integer()
+    Public couleur As String
+
+    Public Sub New(col As String)
+        couleur = col
+    End Sub
+    Public Sub SetCouleur(col As String)
+        couleur = col
+    End Sub
+    Public Function Getcouleur() As String
+        Return couleur
+    End Function
+End Class
+
+Public Class roi
+    Public deplacement As Integer()
+    Public couleur As String
+
+    Public Sub New(col As String)
+        couleur = col
+    End Sub
+    Public Sub SetCouleur(col As String)
+        couleur = col
+    End Sub
+    Public Function Getcouleur() As String
+        Return couleur
+    End Function
 End Class
