e7e17fb6956abce62cec4a4792d1c45149131893
hh1ll <achillebataille@gmail.com>
2021-05-27 17:09:50 +0200

    Update Form1.vb

--- Form1.vb
+++ Form1.vb
@@ -1,12 +1,11 @@
 ﻿Public Class Form1
     Dim whatobject As Object
-    Dim pionblanc1 As pion = New pion({0, 1}, "blanc")
-    Dim pionnoir1 As pion = New pion({0, 6}, "noir")
-
-    'test guthub
-
-
-
+    Dim actualposition As Integer
+    Dim posX As Integer
+    Dim posY As Integer
+    Dim posX_toreach As Integer
+    Dim posY_toreach As Integer
+    Dim bp, wp, bR, bN, bB, bQ, bK, wR, wN, wB, wQ, wK As String
 
     Dim pb As New PictureBox
     Dim board(,) As String =
@@ -15,118 +14,46 @@
         {"__", "__", "__", "__", "__", "__", "__", "__"},
         {"__", "__", "__", "__", "__", "__", "__", "__"},
         {"__", "_", "__", "__", "__", "__", "__", "__"},
-        {"__", "__", "_", "__", "__", "__", "__", "__"},
+        {"__", "__", "__", "__", "__", "__", "__", "__"},
         {"wp", "wp", "wp", "wp", "wp", "wp", "wp", "wp"},
-        {"wR", "wN", "wB", "wQ", "wK", "wB", "wK", "wR"}}
-
-    'rien : 0
-    'pion : 1
-    'cavalier : 2
-    'tour : 3
-    'fou: 4
-    'dame : 5
-    'roi : 6
+        {"wR", "wN", "wB", "wQ", "wK", "wB", "wN", "wR"}}
 
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-        Label2.Text = "bonjour"
-        pb.BackgroundImage = jeudechec.My.Resources.wp
         Me.Height = 1080
         Me.Width = 1920
         Me.FormBorderStyle = FormBorderStyle.FixedSingle
-        tbl_tableau.Width = 900
-        tbl_tableau.Height = 900
-        tbl_tableau.RowCount = 8
-        tbl_tableau.ColumnCount = 8
-        tbl_tableau.BackgroundImageLayout = ImageLayout.Stretch
-        tbl_tableau.Top = 70
-        tbl_tableau.Left = 510
-        tbl_tableau.Controls.Add(Label2, 2, 2)
-        tbl_tableau.ForeColor = Color.Blue
+        'jeudechec.My.Resources.wp
 
+    End Sub
 
-        Case 5
+    Private Function test_positions()
+        Select Case whatobject
+            Case 1
+                If whatobject = bp Or wp Then
+                    If cibleX = posX + 1 Or posX - 1 And cibleY = posY + 1 Or posY - 1 Then
 
-        Case 6
+                    Else
+                        MsgBox("position demandée impossible")
+                    End If
+                End If
+
+            Case 2
+                If whatobject = bN Or wN Then
+                    If  Then
+
+                    End If
+
+                End If
+            Case 3
+
+            Case 4
+
+            Case 5
+
+            Case 6
 
         End Select
-        End Function
-
-End Class
-Public Class pion
-    Public position As Integer()
-    Public deplacement As Integer()
-    Public couleur As String
-
-    Public Sub New(pos As Integer(), col As String)
-        position = pos
-        couleur = col
-    End Sub
-
-    Public Sub SetPosition(pos As Integer())
-        position = pos
-    End Sub
-
-    Public Function GetPosition() As Integer()
-        Return position
     End Function
 
-    Public Sub SetCouleur(col As String)
-        couleur = col
-    End Sub
-
-    Public Function GetCouleur() As String
-        Return couleur
-    End Function
 End Class
 
-Public Class cavalier
-    Public deplacement As Integer()
-    Public couleur As String
-
-    Public Sub New(col As String)
-        couleur = col
-    End Sub
-    Public Sub SetCouleur(col As String)
-        couleur = col
-    End Sub
-    Public Function Getcouleur() As String
-        Return couleur
-    End Function
-End Class
-
-Public Class tour
-    Public deplacement As Integer()
-    Public couleur As String
-
-    Public Sub New(col As String)
-        couleur = col
-    End Sub
-    Public Sub SetCouleur(col As String)
-        couleur = col
-    End Sub
-    Public Function Getcouleur() As String
-        Return couleur
-    End Function
-End Class
-
-Public Class fou
-    Public deplacement As Integer()
-    Public couleur As String
-
-    Public Sub New(col As String)
-        couleur = col
-    End Sub
-    Public Sub SetCouleur(col As String)
-        couleur = col
-    End Sub
-    Public Function Getcouleur() As String
-        Return couleur
-    End Function
-End Class
-
-Public Class dame
-    Public deplacement As Integer()
-    Public couleur As String
-
-End Class
-End Class
