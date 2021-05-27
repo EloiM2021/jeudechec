b39edafecdae44da904c4133837ad2e4b7837ffc
EloiM2021 <81677241+EloiM2021@users.noreply.github.com>
2021-05-27 17:08:21 +0200

    postion début

--- Form1.vb
+++ Form1.vb
@@ -1,69 +1,39 @@
 ﻿Public Class Form1
 
     Dim pb As New PictureBox
+    Dim PieceSelect As String
+    Dim cibleX As Integer
+    Dim cibleY As Integer
+    Dim cibleCase As String
+    Dim posX As Integer
+    Dim posY As String
     Dim board(,) As String =
         {{"bR", "bN", "bB", "bQ", "bK", "bB", "bN", "bR"},
         {"bp", "bp", "bp", "bp", "bp", "bp", "bp", "bp"},
         {"__", "__", "__", "__", "__", "__", "__", "__"},
         {"__", "__", "__", "__", "__", "__", "__", "__"},
-        {"__", "_", "__", "__", "__", "__", "__", "__"},
-        {"__", "__", "_", "__", "__", "__", "__", "__"},
+        {"__", "__", "__", "__", "__", "__", "__", "__"},
+        {"__", "__", "__", "__", "__", "__", "__", "__"},
         {"wp", "wp", "wp", "wp", "wp", "wp", "wp", "wp"},
-        {"wR", "wN", "wB", "wQ", "wK", "wB", "wK", "wR"}}
-
-    'rien : 0
-    'pion : 1
-    'cavalier : 2
-    'tour : 3
-    'fou: 4
-    'dame : 5
-    'roi : 6
+        {"wR", "wN", "wB", "wQ", "wK", "wB", "wN", "wR"}}
 
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-
-        'Me.Height = 1080
-        'Me.Width = 1920
-        'Me.FormBorderStyle = FormBorderStyle.FixedSingle
-
-        'edt_tableau.Text = board
-
+        lbl_attaque.Text = "Attaque en:"
     End Sub
-
-
-
-    Public Class pion
-        Public position As Integer()
-        Public deplacement As Integer()
-        Public couleur As String
-
-        Public Sub New(pos As Integer(), col As String)
-            position = pos
-            couleur = col
-        End Sub
-
-        Public Sub SetPosition(pos As Integer())
-            position = pos
-        End Sub
-
-        Public Function GetPosition() As Integer()
-            Return position
-        End Function
-
-        Public Sub SetCouleur(col As String)
-            couleur = col
-        End Sub
-
-        Public Function GetCouleur() As String
-            Return couleur
-        End Function
-
-
-
-    End Class
 
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles btn_valider.Click
 
+        posX = edt_attaquantX.Text
+        posY = edt_attaquantY.Text
+        PieceSelect = board(posX, posY)
+
+        cibleX = edt_cibleX.Text
+        cibleY = edt_cibleY.Text
+        cibleCase = board(cibleX, cibleY)
+
     End Sub
+    Private Function Affichage()
+        board
+    End Function
 End Class
-' vérifier quel numéro sur la case 
-'ensuite switch case 
+
